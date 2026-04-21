432ab1b56af28695ee66acb6d32a99b83768eddc
한수정 <qoddl159@nate.com>
2026-04-21 16:29:42 +0900

    Fix : 실행 파일 추가 (Run As)

---
+++ src/main/java/com/today/fridge/TodayFridgeApplication.java
@@ -0,0 +1,12 @@
+package com.today.fridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayFridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayFridgeApplication.class, args);
+    }
+}
