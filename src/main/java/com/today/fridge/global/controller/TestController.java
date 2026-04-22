adb8dff789284e0e49ab44ac48014a4c1f1b8688
한수정 <qoddl159@nate.com>
2026-04-22 12:13:15 +0900

    Feat : WebMvcConfig / SecurityConfig 통한CORS 설정 추

---
+++ src/main/java/com/today/fridge/global/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.today.fridge.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "ok";
+    }
+}
