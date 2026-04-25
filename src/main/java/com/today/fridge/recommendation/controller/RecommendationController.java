c2ae347cc35e67487f8e9b0db7ec3664f62e8063
한수정 <qoddl159@nate.com>
2026-04-25 23:26:39 +0900

    Feat : 조건 기반 추천 API 스텁구현

--- src/main/java/com/today/fridge/recommendation/controller/RecommendationController.java
+++ src/main/java/com/today/fridge/recommendation/controller/RecommendationController.java
@@ -1,5 +1,31 @@
 package com.today.fridge.recommendation.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.today.fridge.recommendation.dto.response.RecipeRecommendationResponse;
+import com.today.fridge.recommendation.service.RecommendationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("api/v1/recipes")
+@RequiredArgsConstructor
 public class RecommendationController {
 
+	private final RecommendationService recommendationService;
+	
+	@GetMapping("/recommendations")
+	public ResponseEntity<?> recommend() {
+		Long mockUserId = 1L;
+		
+		List<RecipeRecommendationResponse> result =
+	            recommendationService.recommend(mockUserId);
+
+	    return ResponseEntity.ok(result);
+	}
 }
--- src/main/java/com/today/fridge/recommendation/repository/ConditionCodeRepository.java
+++ src/main/java/com/today/fridge/recommendation/repository/ConditionCodeRepository.java
@@ -1,5 +1,14 @@
 package com.today.fridge.recommendation.repository;
 
-public interface ConditionCodeRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.today.fridge.recommendation.entity.ConditionCode;
+
+public interface ConditionCodeRepository extends JpaRepository<ConditionCode, Long>{
+
+	List<ConditionCode> findByConditionCodeInAndIsActiveTrue(List<String> conditionCodes);
+	
+	List<ConditionCode> findByIsActiveTrue();
 }
