b6c4f2d32dc89fcf7aa828d2d1b96b3d8d83848f
FoggyCaligo <dkdlwkr16@gmail.com>
2026-04-28 11:37:17 +0900

    fix : 실행 안되던 오류 수정

--- src/main/java/com/today/fridge/post/entity/Post.java
+++ src/main/java/com/today/fridge/post/entity/Post.java
@@ -39,6 +39,9 @@ public class Post {
     @Column(name = "report_count")
     private Long reportCount;
 
+    @Column(name = "report_count")
+    private Long reportCount;
+
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
