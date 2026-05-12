843d9c1a836fb16edc08854379156ac47a9bc03c
한수정 <qoddl159@nate.com>
2026-05-12 14:49:32 +0900

    Feat : 유저 조건 저장 로직 추가 및 연결

--- src/main/java/com/today/fridge/recommendation/repository/UserConditionRepository.java
+++ src/main/java/com/today/fridge/recommendation/repository/UserConditionRepository.java
@@ -1,15 +1,20 @@
 package com.today.fridge.recommendation.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.today.fridge.recommendation.entity.ConditionCode;
 import com.today.fridge.recommendation.entity.UserCondition;
 //import com.today.fridge.user.entity.User;
 
 public interface UserConditionRepository extends JpaRepository<UserCondition, Long> {
 
-	List<UserCondition> findByUser_UserIdAndIsActiveTrue(Long userId);
+    List<UserCondition> findByUser_UserIdAndIsActiveTrue(Long userId);
 
-	boolean existsByUser_UserIdAndConditionCode_ConditionId(Long userId, Long conditionId);
+    Optional<UserCondition> findByUser_UserIdAndConditionCode(
+            Long userId,
+            ConditionCode conditionCode
+    );
 }
